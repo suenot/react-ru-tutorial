0d62352395c65f8627ef9399c6ca45778aa93fb8
Eugen Soloviov <suenot@gmail.com>
2016-05-04 07:51:27 +0300

    props

--- js/app.js
+++ js/app.js
@@ -13,6 +13,19 @@ var my_news = [
 	}
 ];
 
+var Article = React.createClass({
+	render: function() {
+		var author = this.props.data.author;
+		var text = this.props.data.text;
+		return (
+			<div className="article">
+				<p className="news__author">{author}:</p>
+				<p className="news__text">{text}</p>
+			</div>
+		)
+	}
+});
+
 var News = React.createClass({
 	render: function() {
 		var data = this.props.data;
@@ -21,8 +34,7 @@ var News = React.createClass({
 			var newsTemplate = data.map(function(item, index) {
 				return (
 					<div key={index}>
-						<p className="news__author">{item.author}:</p>
-						<p className="news__text">{item.text}</p>
+						<Article data={item} />
 					</div>
 				)
 			})
@@ -32,7 +44,7 @@ var News = React.createClass({
 		return (
 			<div className="news">
 				{newsTemplate}
-				<strong className={data.length > 0 ? '':'none'}>Всего новостей: {data.length}</strong>
+				<strong className={'news__count ' + (data.length > 0 ? '':'none') }>Всего новостей: {data.length}</strong>
 			</div>
 		);
 	}
@@ -52,13 +64,13 @@ var App = React.createClass({
 	render: function() {
 		return (
 			<div className="app">
-				Всем привет, я компонент App! Я умею отображать новости.
+				<h3>Новости</h3>
 				<News data={my_news} />
-				<Comments />
 			</div>
 		);
 	}
 });
+
 ReactDOM.render(
 	<App />,
 	document.getElementById('root')
