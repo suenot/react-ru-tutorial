611676649f2278d3e55fa0c84e1dbcd1589a7e5b
Eugen Soloviov <suenot@gmail.com>
2016-05-04 08:00:00 +0300

    propTypes

--- js/app.js
+++ js/app.js
@@ -14,6 +14,12 @@ var my_news = [
 ];
 
 var Article = React.createClass({
+	propTypes: {
+		data: React.PropTypes.shape({
+			author: React.PropTypes.string.isRequired,
+			text: React.PropTypes.string.isRequired
+		})
+	},
 	render: function() {
 		var author = this.props.data.author;
 		var text = this.props.data.text;
@@ -27,6 +33,9 @@ var Article = React.createClass({
 });
 
 var News = React.createClass({
+	propTypes: {
+		data: React.PropTypes.array.isRequired
+	},
 	render: function() {
 		var data = this.props.data;
 		var newsTemplate;
