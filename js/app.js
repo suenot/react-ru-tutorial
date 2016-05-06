46bbcd269ed43b8317b34eb40967f584935c251c
Eugen Soloviov <suenot@gmail.com>
2016-05-06 13:14:55 +0300

    10.1 EventEmitter

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+window.ee = new EventEmitter();
 var my_news = [
 	{
 		author: 'Саша Печкин',
@@ -114,27 +115,21 @@ var Add = React.createClass({
 	},
 	onBtnClickHandler: function(e) {
 		e.preventDefault();
+		var textEl = ReactDOM.findDOMNode(this.refs.text);
 		var author = ReactDOM.findDOMNode(this.refs.author).value;
 		var text = ReactDOM.findDOMNode(this.refs.text).value;
-		alert(author + '\n' + text);
+		var item = [{
+			author: author,
+			text: text,
+			bigText: '...'
+		}];
+		window.ee.emit('News.add', item);
+		textEl.value = '';
+		this.setState({textIsEmpty: true});
 	},
 	onCheckRuleClick: function(e) {
 		this.setState({agreeNotChecked: !this.state.agreeNotChecked});
 	},
-	// onAuthorChange: function(e) {
-	// 	if (e.target.value.trim().length > 0) {
-	// 		this.setState({authorIsEmpty: false})
-	// 	} else {
-	// 		this.setState({authorIsEmpty: true})
-	// 	}
-	// },
-	// onTextChange: function(e) {
-	// 	if (e.target.value.trim().length > 0) {
-	// 		this.setState({textIsEmpty: false})
-	// 	} else {
-	// 		this.setState({textIsEmpty: true})
-	// 	}
-	// },
 	onFieldChange: function(fieldName, e) {
 		if (e.target.value.trim().length > 0) {
 			this.setState({[''+fieldName]:false})
@@ -171,7 +166,7 @@ var Add = React.createClass({
 					onClick={this.onBtnClickHandler}
 					ref='alert_button'
 					disabled={agreeNotChecked || authorIsEmpty || textIsEmpty}>
-					Показать alert
+					Добавить новость
 				</button>
 			</form>
 		);
@@ -179,12 +174,28 @@ var Add = React.createClass({
 });
 
 var App = React.createClass({
+	getInitialState: function() {
+		return {
+			news: my_news
+		};
+	},
+	componentDidMount: function() {
+		var self = this;
+		window.ee.addListener('News.add', function(item) {
+			var nextNews = item.concat(self.state.news);
+			self.setState({news: nextNews});
+		});
+	},
+	componentWillUnmount: function() {
+		window.ee.removeListener('News.add');
+	},
 	render: function() {
+		console.log('render');
 		return (
-			<div className="app">
+			<div className='app'>
 				<Add />
 				<h3>Новости</h3>
-				<News data={my_news} />
+				<News data={this.state.news} />
 			</div>
 		);
 	}
